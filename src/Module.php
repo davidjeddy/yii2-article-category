8b05158beeecd708307f005f169540b3bc46fbba
David E <me@davidjeddy.com>
2015-10-07 00:17:48 -0400

    UPDATED `composer.json` ADDED `./src/Module.php`

---
+++ src/Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace davidjeddy\articlecategory;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'davidjeddy\articlecategory\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
