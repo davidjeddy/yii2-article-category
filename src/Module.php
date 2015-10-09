11107f125b18155178249a7144a0d718f5c12775
David E <me@davidjeddy.com>
2015-10-09 01:30:07 -0400

    version bump

--- src/Module.php
+++ src/Module.php
@@ -6,7 +6,6 @@ class Module extends \yii\base\Module
 {
     public $controllerNamespace = 'davidjeddy\articlecategory\controllers';
     public $defaultRoute        = 'default/index';
-    public $version             = '0.0.2';
 
     public function init()
     {
